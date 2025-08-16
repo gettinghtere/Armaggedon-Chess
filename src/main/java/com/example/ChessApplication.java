674c95c330cf1acf2a9c75fe20e27c23c38d6a22
gettinghtere <idreesfatimie789@gmail.com>
2025-08-16 11:07:08 +0300

    Create ChessApplication.java

---
+++ src/main/java/com/example/ChessApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.example")
+public class ChessApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChessApplication.class, args);
+    }
+}
